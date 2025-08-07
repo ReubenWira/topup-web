5b6e03ae3fe494693cf9e2971a27cc494d38a46b
ReubenWira <68929254+ReubenWira@users.noreply.github.com>
2025-08-07 20:54:05 +0700

    Update register.js

--- register.js
+++ register.js
@@ -10,7 +10,7 @@ document.addEventListener('DOMContentLoaded', () => {
         const data = Object.fromEntries(formData.entries());
 
         try {
-            const response = await fetch('http://localhost:3000/api/register', {
+            const response = await fetch('/api/register', {
                 method: 'POST',
                 headers: { 'Content-Type': 'application/json' },
                 body: JSON.stringify(data),
